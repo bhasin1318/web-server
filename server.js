34b740bc30e8119f2a04ffa9c83d571e5c9b9164
Karan Bhasin <karu.bhasin@gmail.com>
2018-04-03 22:44:04 +0530

    fixed public case

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/about', middleware.requireAuthentication, function (req, res) {
 	res.send('We are cool');
 });
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/Public'));
 
 app.listen(PORT, function () {
 	console.log('express server started at port ' +  PORT);
