a1f7388e08170f394ec2c8e7ce84d9c404040af9
Karan Bhasin <karu.bhasin@gmail.com>
2018-04-03 22:52:05 +0530

    copied code

--- middleware.js
+++ middleware.js
@@ -1,12 +1,14 @@
- var middleware = {
+// Use module exports to expose middleware
+
+var middleware = {
 	requireAuthentication: function (req, res, next) {
-		console.log('Private route hit!')
-		next()
+		console.log('private route hit!');
+		next();
 	},
 	logger: function (req, res, next) {
-		console.log('Request ' + req.method + ' ' + req.originalUrl + 'at ' + new Date().toString())
-		next()
+		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+		next();
 	}
-}
+};
 
-	module.exports = middleware
+module.exports = middleware;
