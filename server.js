bd4fd0bdec90cc3aa490c4df5aba8a6dea9b84ea
Karan Bhasin <karu.bhasin@gmail.com>
2018-04-03 16:39:20 +0530

    Initial Project commit

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express')
+
+var app = express()
+var portToUse = 3000
+var middleware = require('./middleware.js')
+
+
+app.use(middleware.logger)
+// app.get('/', function (req, res) {
+// 	res.send('Hello Express')
+// })
+
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+	res.send('We are cool')
+})
+
+app.use(express.static(__dirname + '/public'))
+
+console.log(__dirname)
+app.listen(portToUse, function () {
+	console.log('express server started at port ' +  portToUse)
+})
