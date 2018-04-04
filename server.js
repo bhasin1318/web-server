8ffb790d989006c938e8b3bf5f20249d5db74168
Karan Bhasin <karu.bhasin@gmail.com>
2018-04-04 12:31:24 +0530

    test final

--- server.js
+++ server.js
@@ -1,26 +1,16 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var open = require('open');
-var path = require('path')
+
 var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
-app.use(express.static(__dirname + '/assets'))
-
-app.get('/', function (req, res) {
-	console.log(__dirname)
-	res.sendFile(path.join(__dirname + '/assets/index.html'));
-});
 
 app.get('/about', middleware.requireAuthentication, function (req, res) {
 	res.send('About Us!');
 });
 
-let url = 'http://localhost:' + PORT;
-open(url,function(err){
-	console.log("Success!")
-});
+app.use(express.static(__dirname + '/assets'));
 
 app.listen(PORT, function () {
 	console.log('Express server started on port ' + PORT + '!');
