6d4e6168101bfee2929f6d3c558c436509335dca
Karan Bhasin <karu.bhasin@gmail.com>
2018-04-04 10:58:53 +0530

    reverted all

--- server.js
+++ server.js
@@ -1,29 +1,17 @@
-// var express = require('express');
-// var app = express();
- var PORT = process.env.PORT || 3000;
-
-// var middleware = require('./middleware.js');
-
-// app.use(middleware.logger);
-
-// app.use(express.static('public'));
-
-// app.get('/', function (req, res) {
-// 	res.send('We r node!');
-// });
-// app.get('/about', middleware.requireAuthentication, function (req, res) {
-// 	res.send('About Us!');
-// });
-
-// app.listen(PORT, function () {
-// 	console.log('Express server started on port ' + PORT + '!');
-// });
-
 var express = require('express');
 var app = express();
+var PORT = process.env.PORT || 3000;
 
-app.use(express.static('/public'));
+var middleware = require('./middleware.js');
 
-app.listen(PORT,function(){
-    console.log('server running on port 3000');
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+	res.send('About Us!');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function () {
+	console.log('Express server started on port ' + PORT + '!');
 });
